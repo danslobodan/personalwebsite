8aa2b2453995147e4ef3c53b1cd56b0516352abf
Slobodan Dan <danastroman@gmail.com>
2020-11-23 22:33:03 +0100

    Formated App.js and index.js to 4 space indentation.

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
 const App = () => {
-  return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <img className="ui medium circular image" src="CV2.jpg" alt="CV picture" />
+    return (
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <img className="ui medium circular image" src="CV2.jpg" alt="CV picture" />
+                </div>
+                <div className="column eight wide">
+                    <div className="ui content">
+                        <h3>Slobodan Dan</h3>
+                        <div className="ui divider"></div>
+                        <p>Hi I'm Slobodan. I'm a full-stack developer. Welcome to my website.</p>
+                        <div className="ui divider"></div>
+                        <p>This is a work in progress but it's a lot of fun building stuff from scratch.</p>
+                        <div className="ui divider"></div>
+                        <p>Have a wonderful day!</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="column eight wide">
-          <div className="ui content">
-            <h3>Slobodan Dan</h3>
-            <div className="ui divider"></div>
-            <p>Hi I'm Slobodan. I'm a full-stack developer. Welcome to my website.</p>
-            <div className="ui divider"></div>
-            <p>This is a work in progress but it's a lot of fun building stuff from scratch.</p>
-            <div className="ui divider"></div>
-            <p>Have a wonderful day!</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;

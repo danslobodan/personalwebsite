d1fa85ccf91681b2acdf5d426f34e4fcd562aed9
Slobodan Dan <danastroman@gmail.com>
2020-11-23 22:48:08 +0100

    Formated text into vertical segments.

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,16 @@ const App = () => {
                     <img className="ui medium circular image" src="CV2.jpg" alt="CV picture" />
                 </div>
                 <div className="column eight wide">
-                    <div className="ui content">
-                        <h3>Slobodan Dan</h3>
-                        <div className="ui divider"></div>
-                        <p>Hi I'm Slobodan. I'm a full-stack developer. Welcome to my website.</p>
-                        <div className="ui divider"></div>
+                    <div className="ui vertical segment">
+                        <div className="ui large header">Slobodan Dan</div>
+                    </div>
+                    <div className="ui vertical segment">
+                        <p className="ui content">Hi I'm Slobodan. I'm a full-stack developer. Welcome to my website.</p>
+                    </div>
+                    <div className="ui vertical segment">
                         <p>This is a work in progress but it's a lot of fun building stuff from scratch.</p>
-                        <div className="ui divider"></div>
+                    </div>
+                    <div className="ui vertical segment">
                         <p>Have a wonderful day!</p>
                     </div>
                 </div>

9b3d7bda10cbad29b0ff566d09624b5cc6e51894
Slobodan Dan <danastroman@gmail.com>
2021-01-08 16:07:31 +0100

    Added GoogleAuth component to App.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import GoogleAuth from './GoogleAuth';
 
 const App = () => {
     return (
         <div className="ui container grid">
+            <div className="ui row">
+                <GoogleAuth />
+            </div>
             <div className="ui row">
                 <div className="column eight wide">
                     <img className="ui medium circular image" src="CV2.jpg" alt="CV picture" />

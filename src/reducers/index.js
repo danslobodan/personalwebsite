8e99223f72928d1b15732c6baecd14f30d59b8ff
Slobodan Dan <danastroman@gmail.com>
2021-01-08 14:57:00 +0100

    Removed fault import statement from reducers index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import authReducer from '../../../LearningReact/streams/client/src/reducers/authReducer';
 import authReducer from './authReducer';
 
 export default combineReducers({

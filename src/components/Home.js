b46ffbad7db53b76676b025f0c36d0d91ab8cdb2
Slobodan Dan <danastroman@gmail.com>
2021-01-09 13:02:00 +0100

    Added an explanation to home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,6 +10,11 @@ const Home = () => {
             <p>
                 Look there's a navigation bar! Press buttonsss!!
             </p>
+            <p>
+                If you've stumbled upon this code by chance, 
+                have in mind that you need to wire your own Client ID and set up
+                your OAuth project on <a href="https://console.developers.google.com/">google developers console</a> page.
+            </p>
         </div>
     );
 };

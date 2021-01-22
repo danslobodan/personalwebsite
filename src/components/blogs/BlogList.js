700bbe9117904e02bbd95f0b74a3409ed7fe3c75
Slobodan Dan <danastroman@gmail.com>
2021-01-22 11:54:26 +0100

    Scafolded BlogList component.

--- src/components/blogs/BlogList.js
+++ src/components/blogs/BlogList.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const BlogList = () => {
+const BlogList = ({ isAdmin }) => {
+
+    const renderAdmin = blog => {
+        
+        if (isAdmin) {
+            return (
+                <div className="right floated content">
+                    <Link 
+                        to={`/blogs/edit/${blog.id}`}
+                        className="ui button primary"
+                    >
+                        Edit
+                    </Link>
+                </div>
+            );
+        };
+    };
     
+    if (isAdmin) {
+        return (
+            <div>BlogListAdmin</div>
+        );
+    }
+
     return (
         <div>BlogList</div>
     );
 };
 
-export default BlogList;
+const mapStateToProps = ({ auth, blogs }) => {
+
+    const { isSignedIn, isAdmin } = auth;
+    return { 
+        isSignedIn,
+        isAdmin,
+        blogs
+    };
+};
+
+export default connect(
+    mapStateToProps
+)(BlogList);

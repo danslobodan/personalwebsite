ddf354f3297de14eb7080f33989b69da1e292931
Slobodan Dan <danastroman@gmail.com>
2021-01-22 18:31:55 +0100

    Wired up reducer from redux-form.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import blogReducer from './blogReducer';
+import { reducer as formReducer } from 'redux-form';
 
 export default combineReducers({
    auth: authReducer,
+   form: formReducer,
    blogs: blogReducer
 });

ac2a509379a5b9f36846936c01fe3f9fd6dfdb9a
Slobodan Dan <danastroman@gmail.com>
2021-01-31 20:37:18 +0100

    Wired and added logic to BlogDelete.

--- client/src/components/blogs/BlogDelete.js
+++ client/src/components/blogs/BlogDelete.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import history from '../../history';
 import Modal from '../Modal';
+import { getBlog, deleteBlog } from '../../actions';
 
 class BlogDelete extends React.Component {
 
+    componentDidMount() {
+        this.props.getBlog(this.props.match.params.id);
+    }
+
     renderActions() {
+
+        const { id } = this.props.match.params;
+
         return (
             <React.Fragment>
-                <button className="ui button primary">Delete</button>
-                <Link to="/" className="ui button">Cancel</Link>
+                <button 
+                    onClick={() => this.props.deleteBlog(id)} 
+                    className="ui button negative">
+                    Delete
+                </button>
+                <Link to="/blogs" className="ui button">Cancel</Link>
             </React.Fragment>
         )
     }
 
+    renderContent() {
+        if (!this.props.blog) {
+            return `Are you sure you want to delete this blog?`   
+        }
+
+        return `Are you sure you want to delete: ${this.props.blog.title}`;
+    }
+
     render() {
+
         return (
             <Modal
                 header="Delete Blog"
-                content="Do you want to delete this blog?"
+                content={this.renderContent()}
                 actions={this.renderActions()}
+                onDismiss={() => history.push('/blogs')}
             />
         )
     }
 }
 
-export default BlogDelete;
+const mapStateToProps = (state, ownProps) => {
+    return { blog: state.blogs[ownProps.match.params.id] };
+}
+
+export default connect(
+    mapStateToProps,
+    { getBlog, deleteBlog }
+)(BlogDelete);

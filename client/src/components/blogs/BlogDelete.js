ee47e2ccddd7905f711e0ab47187c0ad76b0359e
Slobodan Dan <danastroman@gmail.com>
2021-01-31 20:18:03 +0100

    Made Modal customizable and rendered to modal container.

--- client/src/components/blogs/BlogDelete.js
+++ client/src/components/blogs/BlogDelete.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Modal from '../Modal';
 
 class BlogDelete extends React.Component {
 
+    renderActions() {
+        return (
+            <React.Fragment>
+                <button className="ui button primary">Delete</button>
+                <Link to="/" className="ui button">Cancel</Link>
+            </React.Fragment>
+        )
+    }
+
     render() {
         return (
-            <Modal />
+            <Modal
+                header="Delete Blog"
+                content="Do you want to delete this blog?"
+                actions={this.renderActions()}
+            />
         )
     }
 }

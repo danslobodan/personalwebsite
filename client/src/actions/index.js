226f5fe5b0cd5256ce2460caed2ff31eef1c53f1
Slobodan Dan <danastroman@gmail.com>
2021-01-31 18:26:22 +0100

    Added getBlogs, getBlog, createBlog and editBlog action creators.

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,12 @@ import {
     SIGN_IN, 
     SIGN_OUT,
     CREATE_BLOG,
-    EDIT_BLOG
+    EDIT_BLOG,
+    GET_BLOGS,
+    GET_BLOG
 } from './types';
+import blogs from '../apis/blogs';
+import history from '../history';
 
 export const signIn = (userId) => {
     return {
@@ -18,16 +22,25 @@ export const signOut = () => {
     }
 }
 
-export const createBlog = formValues => {
-    return {
-        type: CREATE_BLOG,
-        payload: formValues
-    }
+export const getBlogs = () => async dispatch => {
+    const response = await blogs.get('/blogs');
+    dispatch({ type: GET_BLOGS, payload: response.data });
 }
 
-export const editBlog = formValues => {
-    return {
-        type: EDIT_BLOG,
-        payload: formValues
-    }
+export const getBlog = id => async dispatch => {
+    console.log(id);
+    const response = await blogs.get(`/blogs/${id}`);
+    dispatch({ type: GET_BLOG, payload: response.data });
+}
+
+export const createBlog = formValues => async dispatch => {
+    const response = await blogs.post('/blogs', formValues);
+    dispatch({ type: CREATE_BLOG, payload: response.data });
+    history.push('/');
+}
+
+export const editBlog = (id, formValues) => async dispatch => {
+    const response = await blogs.patch(`/blogs/${id}`, formValues);
+    dispatch({ type: EDIT_BLOG, payload: response.data });
+    history.push('/');
 }

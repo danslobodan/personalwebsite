f570067302c7eebf691c3ef1d2b0162a5af5e57c
Slobodan Dan <danastroman@gmail.com>
2021-01-31 18:25:20 +0100

    Added GET_BLOG and GET_BLOGS action types.

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,7 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
 
+export const GET_BLOGS = 'GET_BLOGS';
+export const GET_BLOG = 'GET_BLOG';
 export const CREATE_BLOG = 'CREATE_BLOG';
 export const EDIT_BLOG = 'EDIT_BLOG';

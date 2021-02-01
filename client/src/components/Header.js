02144f92a9ec37a3e85e76b0c7d2c83f23effb31
Slobodan Dan <danastroman@gmail.com>
2021-02-01 15:22:34 +0100

    Redesigned header.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,18 +4,21 @@ import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
     return (
-        <div className="ui secondary pointing menu">
-            <Link to="/" className="item">
-                Home
-            </Link>
-            <Link to="/blogs" className="item">
-                Blogs
-            </Link>
-            <div className="right menu">
-                <Link to="/about" className="item">
-                    About
+        <div className="ui vertical segment">
+            <div className="ui inverted block huge header">The True Architect</div>
+            <div className="ui secondary menu">
+                <Link to="/" className="item">
+                    Home
                 </Link>
-                <GoogleAuth />
+                <Link to="/blogs" className="item">
+                    Blogs
+                </Link>
+                <div className="right menu">
+                    <Link to="/about" className="item">
+                        About
+                    </Link>
+                    <GoogleAuth />
+                </div>
             </div>
         </div>
     );

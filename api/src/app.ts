171510249318f7e0a4cb48c22b96b7f833709868
Slobodan Dan <danastroman@gmail.com>
2021-03-09 23:05:28 +0100

    Added cors to api and redirected client to port 5000.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
+
 import { NotFoundError } from './errors/notFoundError';
 import { errorHandler } from './middlewares/errorHandler';
 import { createBlogRouter } from './routes/blogs/createBlog';
@@ -8,6 +10,7 @@ import { updateBlogRouter } from './routes/blogs/updateBlog';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get('/api', async (req: Request, res: Response) => {
     return res.status(200).send({ message: 'Health Check OK' });

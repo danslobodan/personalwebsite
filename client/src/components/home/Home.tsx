4a10e49bee44cbea063b96469eb90836537e8ecc
Slobodan Dan <danastroman@gmail.com>
2021-03-11 21:34:13 +0100

    Styled content on the home page.

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -4,44 +4,41 @@ import { Tenet } from './Tenet';
 const Home: React.FC = () => {
     return (
         <div className='container'>
-            <div className='row'>
-                <div>
+            <div className='row featurette'>
+                <p className='lead'>
                     The true architect is an ideal I have created to strive to.
-                    It's not meant to be achieved.
-                </div>
-                <div>It encompases five tenets:</div>
+                    It's not meant to be achieved. It comprises five tenets.
+                </p>
             </div>
-            <div className='row'>
-                <Tenet title='Master Programmer'>
-                    The true architect always practices creating and modifying
-                    models. He seeks to understand the business, the users and
-                    his team, to unite the three in the model they all
-                    understand and agree upon.
-                </Tenet>
-                <Tenet title='Inventive Designer'>
-                    The true architect always practices creating and modifying
-                    models. He seeks to understand the business, the users and
-                    his team, to unite the three in the model they all
-                    understand and agree upon.
-                </Tenet>
-                <Tenet title='Supportive Manager'>
-                    The true architect understands that communication,
-                    cooperation and trust are at the foundation of success, and
-                    that the team is the keystone. He works to empower the team,
-                    and help them find purpose and reach autonomy and mastery.
-                </Tenet>
-                <Tenet title='Patient Teacher'>
-                    The true architect continuously teaches his team everything
-                    he knows, repeating the same lessons when he needs to, and
-                    points to the correct sources of knowledge. He keeps
-                    improving his teaching techniques, motivates his pupils to
-                    learn and never stops learning himself.
-                </Tenet>
-                <Tenet title='Adept Psychologist'>
-                    The true architect remembers that people create software. He
-                    learns about people as much as he learns about technology.
-                </Tenet>
-            </div>
+            <Tenet title='Master Programmer'>
+                The true architect always practices creating and modifying
+                models. He seeks to understand the business, the users and his
+                team, to unite the three in the model they all understand and
+                agree upon.
+            </Tenet>
+            <Tenet title='Inventive Designer'>
+                The true architect always practices creating and modifying
+                models. He seeks to understand the business, the users and his
+                team, to unite the three in the model they all understand and
+                agree upon.
+            </Tenet>
+            <Tenet title='Supportive Manager'>
+                The true architect understands that communication, cooperation
+                and trust are at the foundation of success, and that the team is
+                the keystone. He works to empower the team, and help them find
+                purpose and reach autonomy and mastery.
+            </Tenet>
+            <Tenet title='Patient Teacher'>
+                The true architect continuously teaches his team everything he
+                knows, repeating the same lessons when he needs to, and points
+                to the correct sources of knowledge. He keeps improving his
+                teaching techniques, motivates his pupils to learn and never
+                stops learning himself.
+            </Tenet>
+            <Tenet title='Adept Psychologist'>
+                The true architect remembers that people create software. He
+                learns about people as much as he learns about technology.
+            </Tenet>
         </div>
     );
 };
--- client/src/components/home/Tenet.tsx
+++ client/src/components/home/Tenet.tsx
@@ -1,18 +1,27 @@
-interface TProps {
+import './Tenet.css';
+
+interface Props {
     title: string;
 }
 
-export const Tenet: React.FC<TProps> = ({ title, children }) => {
+export const Tenet: React.FC<Props> = ({ title, children }) => {
     return (
-        <div className='col-lg-4'>
-            <img
-                className='rounded-circle'
-                src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
-                width='140'
-                height='140'
-            />
-            <h2>{title}</h2>
-            <p>{children}</p>
-        </div>
+        <>
+            <hr className='featurette-divider' />
+            <div className='row featurette'>
+                <div className='col-md-2'>
+                    <img
+                        className='rounded-circle'
+                        src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
+                        width='150'
+                        height='150'
+                    />
+                </div>
+                <div className='col-md-10'>
+                    <h2 className='featurette-heading'>{title}</h2>
+                    <p className='lead'>{children}</p>
+                </div>
+            </div>
+        </>
     );
 };

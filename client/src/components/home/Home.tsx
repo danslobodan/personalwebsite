d70a15ce14113f6e5f086b5325e064f6956b2390
Slobodan Dan <danastroman@gmail.com>
2021-03-18 00:06:03 +0100

    Restyled Home page.

--- client/src/components/home/Home.tsx
+++ client/src/components/home/Home.tsx
@@ -10,31 +10,31 @@ const Home: React.FC = () => {
                     It's not meant to be achieved. It comprises five tenets.
                 </p>
             </div>
-            <Tenet title='Master Programmer'>
+            <Tenet imageLeft title='Master Programmer'>
                 He can singlehandedly create a prototype of the product he
                 designed, including writing code, setting up infrastructure and
                 deployment pipeline, and writing documentation.
             </Tenet>
-            <Tenet title='Inventive Designer'>
+            <Tenet imageLeft={false} title='Inventive Designer'>
                 The true architect always practices creating and modifying
                 models. He seeks to understand the business, the users and his
                 team, to unite the three in the model they all understand and
                 agree upon.
             </Tenet>
-            <Tenet title='Supportive Manager'>
+            <Tenet imageLeft title='Supportive Manager'>
                 The true architect understands that communication, cooperation
                 and trust are at the foundation of success, and that the team is
                 the keystone. He works to empower the team, and help them find
                 purpose and reach autonomy and mastery.
             </Tenet>
-            <Tenet title='Patient Teacher'>
+            <Tenet imageLeft={false} title='Patient Teacher'>
                 The true architect continuously teaches his team everything he
                 knows, repeating the same lessons when he needs to, and points
                 to the correct sources of knowledge. He keeps improving his
                 teaching techniques, motivates his pupils to learn and never
                 stops learning himself.
             </Tenet>
-            <Tenet title='Adept Psychologist'>
+            <Tenet imageLeft title='Adept Psychologist'>
                 The true architect remembers that people create software. He
                 learns about people as much as he learns about technology.
             </Tenet>
--- client/src/components/home/Tenet.tsx
+++ client/src/components/home/Tenet.tsx
@@ -1,24 +1,43 @@
 interface Props {
     title: string;
+    imageLeft: boolean;
 }
 
-export const Tenet: React.FC<Props> = ({ title, children }) => {
+export const Tenet: React.FC<Props> = ({ title, imageLeft, children }) => {
+    const renderFlip = (image: boolean) => {
+        if (image) {
+            return renderImage();
+        }
+
+        return renderTenet();
+    };
+
+    const renderImage = () => {
+        return (
+            <img
+                className='rounded'
+                src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
+                width='300'
+                height='300'
+            />
+        );
+    };
+
+    const renderTenet = () => {
+        return (
+            <>
+                <h2 className='featurette-heading'>{title}</h2>
+                <p className='lead'>{children}</p>
+            </>
+        );
+    };
+
     return (
         <>
             <hr className='featurette-divider' />
             <div className='row featurette'>
-                <div className='col-md-2'>
-                    <img
-                        className='rounded-circle'
-                        src='data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=='
-                        width='150'
-                        height='150'
-                    />
-                </div>
-                <div className='col-md-10'>
-                    <h2 className='featurette-heading'>{title}</h2>
-                    <p className='lead'>{children}</p>
-                </div>
+                <div className='col-md-6'>{renderFlip(imageLeft)}</div>
+                <div className='col-md-6'>{renderFlip(!imageLeft)}</div>
             </div>
         </>
     );

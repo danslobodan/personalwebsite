7ce68462ff2466547f8d2bce4def3e45a711e642
Slobodan Dan <danastroman@gmail.com>
2021-03-17 06:14:10 +0100

    Logging responses and response time.

--- api/src/middlewares/routeLogger.ts
+++ api/src/middlewares/routeLogger.ts
@@ -1,10 +1,36 @@
 import { Request, Response, NextFunction } from 'express';
 
+const getDurationInMilliseconds = (start: [number, number]) => {
+    const NS_PER_SEC = 1e9;
+    const NS_TO_MS = 1e6;
+    const diff = process.hrtime(start);
+
+    return (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MS;
+};
+
+const formatOutput = (url: string, statusCode: number, miliseconds: number) => {
+    console.log(
+        `route ${url} response ${statusCode} miliseconds ${miliseconds}`
+    );
+};
+
 export const routeLogger = (
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
-    console.log('route:', req.url, 'body', req.body);
+    const route = req.url;
+    const start = process.hrtime();
+
+    console.log('route', req.url);
+
+    res.on('finish', () => {
+        const miliseconds = getDurationInMilliseconds(start);
+        formatOutput(route, res.statusCode, miliseconds);
+    });
+    res.on('close', () => {
+        const miliseconds = getDurationInMilliseconds(start);
+        formatOutput(route, res.statusCode, miliseconds);
+    });
     next();
 };

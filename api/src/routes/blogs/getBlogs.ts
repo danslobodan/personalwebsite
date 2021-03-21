eae8a1326b436bb63b50ab8264b258c1f021ba47
Slobodan Dan <danastroman@gmail.com>
2021-03-21 23:38:55 +0100

    Getting blogs from mediums rss feed.

--- api/src/routes/blogs/getBlogs.ts
+++ api/src/routes/blogs/getBlogs.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
-import { Blog } from '../../models/blog';
+import Parser from 'rss-parser';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
-    const blogs = await Blog.find({});
+    const parser = new Parser();
+    const mediumResponse = await parser.parseURL(
+        'https://medium.com/feed/@danastroman'
+    );
+    const blogs = mediumResponse.items.map((item) => {
+        return {
+            id: item.guid,
+            title: item.title,
+            link: item.link,
+            img: item.imgUrl,
+            date: item.pubDate,
+        };
+    });
     res.send(blogs);
 });
 

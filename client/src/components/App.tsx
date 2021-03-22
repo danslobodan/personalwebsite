b44bcac93d5d7cc7532fda3e8dfcd742746d7114
Slobodan Dan <danastroman@gmail.com>
2021-03-22 21:43:02 +0100

    Removed EditForm from routes.

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -6,7 +6,6 @@ import { Router, Route } from 'react-router-dom';
 import Header from './nav/Header';
 import Home from './home/Home';
 import BlogList from './blogs/BlogList';
-import BlogEdit from './blogs/BlogEdit';
 import About from './about/About';
 import history from '../history';
 
@@ -18,7 +17,6 @@ const App: React.FC = () => {
                 <div className='container'>
                     <Route path='/' exact component={Home} />
                     <Route path='/blogs' exact component={BlogList} />
-                    <Route path='/blogs/edit/:id' exact component={BlogEdit} />
                     <Route path='/about' exact component={About} />
                 </div>
             </main>

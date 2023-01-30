10de908c5c905b1774564f38552abbb2102669ed
Slobodan Dan <danastroman@gmail.com>
2023-01-30 14:23:08 +0100

    Changed bullet style in CV.

--- components/cv/Skills.tsx
+++ components/cv/Skills.tsx
@@ -22,7 +22,7 @@ interface SkillSectionProps {
 
 const SkillSection: React.FC<SkillSectionProps> = ({ skills }) => {
     return (
-        <ul style={{ paddingLeft: "25px" }}>
+        <ul style={{ paddingLeft: "25px", listStyleType: "circle" }}>
             {skills.map((skill) => {
                 return (
                     <li key={skill} style={{ marginBottom: "5px" }}>

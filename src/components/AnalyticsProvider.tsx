c14a88d89204adc856c3554b86ddcd9ad5464f6b
Slobodan Dan <slobodan.s.dan@gmail.com>
2024-05-21 20:43:45 +0200

    Changed configuration for posthog.

--- src/components/AnalyticsProvider.tsx
+++ src/components/AnalyticsProvider.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-import posthog from 'posthog-js';
-import { PostHogProvider } from 'posthog-js/react';
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
     // checks that we are client-side
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
         api_host:
-            process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+            process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://eu.i.posthog.com",
         capture_pageview: false,
         loaded: (posthog) => {
-            if (process.env.NODE_ENV === 'development') posthog.debug(); // debug mode in development
+            if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
         },
     });
 }
